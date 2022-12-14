0cc34abb5f7f1ff54a6eea580d317f0432d4fc43
Ellie Schaffer <ellie@collegeplannerpro.com>
2022-12-13 20:35:57 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const core = require('@actions/core');
-const notifySlack = require('./notify-slack');
+const github = require('@actions/github');
 
-notifySlack(core);
+const slackWebhookUrl = core.getInput('slack-webhook-url');
+const eventPayload = github.context.payload;
+eventPayload.repository.name;
+const message = `
+    On *${eventPayload.repository.name}* ran *${github.context.workflow}* (${eventPayload.pull_request.title}) for ${github.event.head_commit.author.name || github.context.actor}: ${github.context.job.status} [<${github.context.serverUrl}/${github.context.repo}/actions/runs/${github.context.runId}|result>]
+`;
+core.setOutput('message', message);
