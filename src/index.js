a4181fd8d67fae597068f3f83cddcd101f0c549e
Ellie Schaffer <ellie@collegeplannerpro.com>
2022-12-13 20:37:43 -0500

    patch

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const github = require('@actions/github');
 
 const slackWebhookUrl = core.getInput('slack-webhook-url');
 const eventPayload = github.context.payload;
-eventPayload.repository.name;
+const pullRequest = eventPayload.pull_request;
+const pullRequestTitle = pullRequest && pullRequest.title;
+
 const message = `
-    On *${eventPayload.repository.name}* ran *${github.context.workflow}* (${eventPayload.pull_request.title}) for ${github.event.head_commit.author.name || github.context.actor}: ${github.context.job.status} [<${github.context.serverUrl}/${github.context.repo}/actions/runs/${github.context.runId}|result>]
+    On *${eventPayload.repository.name}* ran *${github.context.workflow}* (${pullRequestTitle}) for ${github.event.head_commit.author.name || github.context.actor}: ${github.context.job.status} [<${github.context.serverUrl}/${github.context.repo}/actions/runs/${github.context.runId}|result>]
 `;
 core.setOutput('message', message);
