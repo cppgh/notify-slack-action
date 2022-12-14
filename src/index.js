77a59dcf63bcfb57d87f338512d1ba72b2247210
Ellie Schaffer <ellie@collegeplannerpro.com>
2022-12-13 20:58:43 -0500

    build it out

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const slackWebhookUrl = core.getInput('slack-webhook-url');
+const jobStatus = core.getInput('job-status');
 const eventPayload = github.context.payload;
 const pullRequest = eventPayload.pull_request;
 const pullRequestTitle = pullRequest && pullRequest.title;
 const headCommit = eventPayload.head_commit;
 const headCommitAuthor = headCommit && headCommit.author.name;
-const headCommitMessage = headCommit && headCommit.message;
+
+// take the first line
+const headCommitMessage = ((headCommit && headCommit.message) || '').split("\n")[0];
 
 const message = `
-    On *${eventPayload.repository.name}* ran *${github.context.workflow}* (${pullRequestTitle || headCommitMessage}) for ${headCommitAuthor}: JOB_STATUS [<${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}|result>]
-`;
+    On *${eventPayload.repository.name}* ran *${github.context.workflow}*
+    (${pullRequestTitle || headCommitMessage}) for ${headCommitAuthor}:
+    ${jobStatus}
+    [<${github.context.serverUrl}/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${github.context.runId}|result>]
+`.replace("\n", ' ').replace(/ +/g, ' ');
 console.log(github.context);
-core.setOutput('message', message);
+const payload = {
+    text: `Build result for ${github.context.repo.repo}`,
+    blocks: [
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: message
+            }
+        }
+    ]
+};
+core.setOutput('payload', JSON.stringify(payload));
